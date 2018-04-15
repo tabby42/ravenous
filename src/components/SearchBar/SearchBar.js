47aeec713bae7806de8fdf5dffe4b9ca93911d8e
tabby42 <irene.theiss@gmx.at>
2018-04-15 11:03:15 +0200

    add render function

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 const sortByOptions = {
-	"Best Match": "best_match"
-	"Highest Rated": "rating"
+	"Best Match": "best_match",
+	"Highest Rated": "rating",
 	"Most Reviewed": "review_count"
 };
 
@@ -19,6 +19,23 @@ class SearchBar extends Component {
 	}
 
 	render() {
-		
+		return(
+			<div className="SearchBar">
+			  <div className="SearchBar-sort-options">
+			    <ul>
+			      {this.renderSortByOptions()}
+			    </ul>
+			  </div>
+			  <div className="SearchBar-fields">
+			    <input placeholder="Search Businesses" />
+			    <input placeholder="Where?" />
+			  </div>
+			  <div className="SearchBar-submit">
+			    <a>Let's Go</a>
+			  </div>
+			</div>
+		);
 	}
 }
+
+export default SearchBar;
