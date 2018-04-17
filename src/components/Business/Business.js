089a6c44b8d626f5baf377f64a2bb1edd1dffa10
tabby42 <irene.theiss@gmx.at>
2018-04-17 17:07:35 +0200

    modify display of business information

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -6,19 +6,29 @@ class Business extends Component {
 		return (
 			<div className="Business">
 			  <div className="image-container">
-			    <img src={this.props.business.imageSrc} alt={this.props.business.name}/>
+			  	<a href={this.props.business.url} target="_blank">
+			    	<img src={this.props.business.imageSrc} alt={this.props.business.name}/>
+		    	</a>
 			  </div>
 			  <h2>{this.props.business.name}</h2>
 			  <div className="Business-information">
 			    <div className="Business-address">
-			      <p>{this.props.business.address}</p>
-			      <p>{this.props.business.city}</p>
-			      <p>{this.props.business.state} {this.props.business.zipCode} </p>
+			      <p>
+		      		{this.props.business.address.map( (item, index) => <span key={index}>{item}<br /></span> )}
+			      </p>
+			      <p>
+			      	{this.props.business.phone}
+			      </p>
 			    </div>
 			    <div className="Business-reviews">
-			      <h3>{this.props.business.categories}</h3>
+			      <h3>
+			      	{this.props.business.categories.map( category =>
+			      		<span key={category.alias}>{category.title} | </span>
+		      		)}
+			      </h3>
 			      <h3 className="rating">{this.props.business.rating} stars</h3>
 			      <p>{this.props.business.reviewCount} reviews</p>
+			      <p>{this.props.business.isClosed ? 'closed' : 'open'}</p>
 			    </div>
 			  </div>
 			</div>
