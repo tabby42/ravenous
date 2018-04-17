75fa2900449c7ed253dac187cbfbc321b9bf5b1c
tabby42 <irene.theiss@gmx.at>
2018-04-17 14:38:12 +0200

    add key property to List of Businesses

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,7 +6,9 @@ class BusinessList extends Component {
 	render() {
 		return (
 			<div className="BusinessList">
-			  {this.props.businesses.map( business => { return <Business business={business}/>; } )}
+			  {this.props.businesses.map( business => { 
+			  	return <Business business={business} key={business.id}/>; 
+			  } )}
 			</div>
 		);
 	}
